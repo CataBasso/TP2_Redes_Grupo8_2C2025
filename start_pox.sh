0985a161f13f9c363789dfd1064772d6af5c2118
Celeste <90164368+celestedebe@users.noreply.github.com>
2025-11-21 13:24:33 -0300

    Ajuste en start_posh.sh

--- start_pox.sh
+++ start_pox.sh
@@ -12,4 +12,4 @@ echo "Logs se guardarán en: $LOG_FILE"
 echo "Presiona Ctrl+C para detener"
 
 ./pox.py log.level --DEBUG l2_learning_custom 2>&1 | tee "$LOG_FILE"
-
+#python3 ./pox.py log.level --DEBUG l2_learning_custom 2>&1 | tee "$LOG_FILE" 
